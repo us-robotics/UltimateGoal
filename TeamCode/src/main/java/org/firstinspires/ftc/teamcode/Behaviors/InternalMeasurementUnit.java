79f9b46bec79dfdb9f35e0ab81f71f7d59afbbdc
MMXXXVIII <henhaodegaigai@gmail.com>
2020-11-11 00:51:27 -0500

    Enhanced drivetrain movement code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Behaviors/InternalMeasurementUnit.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Behaviors;
+
+import FTCEngine.Core.Behavior;
+import FTCEngine.Core.OpModeBase;
+
+public class InternalMeasurementUnit extends Behavior
+{
+	/**
+	 * NOTE: Do not configure the electronics in the constructor, do them in the awake method!
+	 *
+	 * @param opMode
+	 */
+	public InternalMeasurementUnit(OpModeBase opMode)
+	{
+		super(opMode);
+	}
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Behaviors/MecanumDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Behaviors/MecanumDrivetrain.java
@@ -30,13 +30,11 @@ public class MecanumDrivetrain extends Behavior
 		backLeft = hardwareMap.dcMotor.get("backLeft");
 		backRight = hardwareMap.dcMotor.get("backRight");
 
-		frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-		backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+		frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+		backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
-		frontLeft.setPower(0d);
-		frontRight.setPower(0d);
-		backLeft.setPower(0d);
-		backRight.setPower(0d);
+		imu = opMode.getBehavior(InternalMeasurementUnit.class);
+		setRawVelocities(Vector2.zero, 0f);
 	}
 
 	private DcMotor frontRight;
@@ -44,27 +42,68 @@ public class MecanumDrivetrain extends Behavior
 	private DcMotor backRight;
 	private DcMotor backLeft;
 
+	private InternalMeasurementUnit imu;
+	private final float[] powers = new float[4];
+
 	@Override
 	public void update()
 	{
 		super.update();
+
+		if (getIsAuto()) return; //Temporary, TODO: Add auto mode processing
 		Input input = opMode.getHelper(Input.class);
 
-		Vector2 movementInput = input.getVector(Input.Source.CONTROLLER_1, Input.Button.LEFT_JOYSTICK);
-		float rotationInput = input.getVector(Input.Source.CONTROLLER_1, Input.Button.RIGHT_JOYSTICK).x;
+		Vector2 positionalInput = input.getVector(Input.Source.CONTROLLER_1, Input.Button.LEFT_JOYSTICK).normalize();
+		float rotationalInput = input.getVector(Input.Source.CONTROLLER_1, Input.Button.RIGHT_JOYSTICK).x;
 
-		//Process input for smoother control
-		movementInput = movementInput.normalize().mul(movementInput.getMagnitude());
-		rotationInput *= Math.abs(rotationInput);
+		//Process input for smoother control by interpolating a polynomial curve (can use higher power if more precision is needed)
+		positionalInput = positionalInput.mul(positionalInput.getMagnitude());
+		rotationalInput *= Math.abs(rotationalInput);
 
-		//Set zero power behavior
-		boolean hasMovement = !movementInput.equals(Vector2.zero) || !Mathf.almostEquals(rotationInput, 0f);
-		setZeroPowerBehavior(hasMovement ? DcMotor.ZeroPowerBehavior.FLOAT : DcMotor.ZeroPowerBehavior.BRAKE);
+		setMovements(positionalInput, rotationalInput);
+	}
 
-		frontRight.setPower(-movementInput.y + movementInput.x + rotationInput);
-		frontLeft.setPower(-movementInput.y - movementInput.x - rotationInput);
-		backRight.setPower(-movementInput.y - movementInput.x + rotationInput);
-		backLeft.setPower(-movementInput.y + movementInput.x - rotationInput);
+	private void setMovements(Vector2 positionalMovement, float rotationalMovement)
+	{
+		//If no rotational input, then IMU is used to counterbalance hardware inaccuracy to drive straight
+		if (imu != null && Mathf.almostEquals(rotationalMovement, 0f))
+		{
+			//TODO: IMU correction
+		}
+		else setRawVelocities(positionalMovement, rotationalMovement);
+	}
+
+	private void setRawVelocities(Vector2 localDirection, float angularDelta)
+	{
+		localDirection = localDirection.normalize();
+		angularDelta = Mathf.clamp(angularDelta, -1f, 1f);
+
+		powers[0] = localDirection.y - localDirection.x - angularDelta;
+		powers[1] = localDirection.y + localDirection.x + angularDelta;
+		powers[2] = localDirection.y + localDirection.x - angularDelta;
+		powers[3] = localDirection.y - localDirection.x + angularDelta;
+
+		float max = -Float.MAX_VALUE;
+
+		for (int i = 0; i < powers.length; i++)
+		{
+			float power = Math.abs(powers[i]);
+			max = Math.max(max, power);
+		}
+
+		if (max > 1f)
+		{
+			//Scales all powers down by a multiplier if one power is higher than 1
+			for (int i = 0; i < powers.length; i++) powers[i] /= max;
+		}
+
+		boolean hasPower = !Mathf.almostEquals(max, 0f); //Switches between the two zero power behaviors to brake
+		setZeroPowerBehavior(hasPower ? DcMotor.ZeroPowerBehavior.FLOAT : DcMotor.ZeroPowerBehavior.BRAKE);
+
+		frontRight.setPower(powers[0]);
+		frontLeft.setPower(powers[1]);
+		backRight.setPower(powers[2]);
+		backLeft.setPower(powers[3]);
 	}
 
 	private void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior behavior) //what does this do?
