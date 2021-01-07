b4859753079e54b6fc3f7775aa1ec15654e60ce6
MMXXXVIII <henhaodegaigai@gmail.com>
2021-01-07 17:06:52 -0500

    Tuning auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/MecanumI4Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/MecanumI4Auto.java
@@ -34,6 +34,10 @@ public class MecanumI4Auto extends AutoOpModeBase
 		DrivetrainAuto drivetrain = getBehavior(DrivetrainAuto.class);
 		WobbleGrabberAuto wobbleGrabber = getBehavior(WobbleGrabberAuto.class);
 
+		execute(drivetrain, (DrivetrainAuto.Job)new DrivetrainAuto.Move(new Vector2(0f, 24f * 3f)));
+
+		if (true) return;
+
 		grabWobble(10f, 9f);
 
 		execute(drivetrain, (DrivetrainAuto.Job)new DrivetrainAuto.Move(new Vector2(0f, 94f)));
