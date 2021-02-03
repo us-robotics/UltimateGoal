968883ff4f90b4739c0c122205cd0b8a3c25fd9f
Holden Neely <hneely22@us.edu>
2021-02-03 16:04:38 -0500

    new grabWobble method thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iteration4/SequenceB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iteration4/SequenceB.java
@@ -6,6 +6,7 @@ import org.firstinspires.ftc.teamcode.Behaviors.Launcher;
 import org.firstinspires.ftc.teamcode.Behaviors.WobbleGrabber;
 
 import FTCEngine.Core.OpModeBase;
+import FTCEngine.Math.Vector2;
 
 public class SequenceB extends CommonSequence
 {
@@ -15,16 +16,33 @@ public class SequenceB extends CommonSequence
 	}
 
 	@Override
-	protected void queueJobs()
-	{
+	protected void queueJobs() {
 		Drivetrain drivetrain = opMode.getBehavior(Drivetrain.class);
 		WobbleGrabber wobbleGrabber = opMode.getBehavior(WobbleGrabber.class);
 		Launcher launcher = opMode.getBehavior(Launcher.class);
 		Intake intake = opMode.getBehavior(Intake.class);
 
-		grabWobble(10f, 9f);
+		grabWobble(10f, 9f); //ends with wobble high
 
-		//execute(drivetrain, new Drivetrain.Move(new Vector2(0f, 92f)));
-		//execute();
+		//drive to square, let go, reset on wall
+		execute(drivetrain, new Drivetrain.Move(new Vector2(0f, 100f)));
+		execute(wobbleGrabber, new WobbleGrabber.Move(WobbleGrabber.Position.GRAB));
+		execute(wobbleGrabber, new WobbleGrabber.Grab(false));
+		execute(wobbleGrabber, new WobbleGrabber.Move(WobbleGrabber.Position.FOLD));
+		execute(drivetrain, new Drivetrain.Drive(new Vector2(-1f, 0f), .5f));
+		wait(.4f);
+
+		execute(drivetrain, new Drivetrain.Move(new Vector2(8f, 0)));
+		execute(drivetrain, new Drivetrain.Move(new Vector2(0f, -90f)));
+		execute(drivetrain, new Drivetrain.Drive(new Vector2(0f, -1f), .5f));
+		wait(.4f);
+
+		grabWobble(4f, 9f);
+
+		execute(drivetrain, new Drivetrain.Move(new Vector2(0f, 76f)));
+		execute(drivetrain, new Drivetrain.Rotate(90f));
+		execute(drivetrain, new Drivetrain.Move(new Vector2(-16f, 0f)));
+		execute(drivetrain, new Drivetrain.Rotate(90f));
+		execute(drivetrain, new Drivetrain.Drive(new Vector2(1f, 0f)));
 	}
 }
