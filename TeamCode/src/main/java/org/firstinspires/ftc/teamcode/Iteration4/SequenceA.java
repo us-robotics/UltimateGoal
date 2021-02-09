9da209b3bdadcfa4d5d49bd8eafe0be0c077dbc8
Holden Neely <hneely22@us.edu>
2021-02-09 16:07:30 -0500

    rough draft of b and a auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iteration4/SequenceA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iteration4/SequenceA.java
@@ -27,39 +27,22 @@ public class SequenceA extends CommonSequence
 
 		//grabWobble(10f, 9f);
 
-		execute(drivetrain, new Drivetrain.Move(new Vector2(0f, 104f)));
+		execute(drivetrain, new Drivetrain.Move(new Vector2(0f, 64f)));
 
 
 		execute(drivetrain, new Drivetrain.Move(new Vector2(20f, 0f)));
 		execute(drivetrain, new Drivetrain.Rotate(-90f));
 		execute(drivetrain, new Drivetrain.Rotate(-90f));
 
+		//execute(wobbleGrabber, new WobbleGrabber.Move(WobbleGrabber.Position.GRAB));
+		//execute(wobbleGrabber, new WobbleGrabber.Grab(false));
 		wait(0.4f);
 
-		//Hit wall to Reset Y
-		execute(drivetrain, new Drivetrain.Move(new Vector2(0f, -12f)));
-		execute(drivetrain, new Drivetrain.Drive(new Vector2(0f, -1f), 0.5f));
-		wait(0.7f);
-
-		execute(drivetrain, new Drivetrain.Drive(Vector2.zero));
-		execute(drivetrain, new Drivetrain.Reset());
-
+		execute(drivetrain, new Drivetrain.Move(new Vector2(0f, 20f)));
+		execute(drivetrain, new Drivetrain.Move(new Vector2(24f, 0f)));
 		execute(launcher, new Launcher.Prime(Launcher.HIGH_POWER - 0.02f, true));
-		execute(drivetrain, new Drivetrain.Move(new Vector2(-10f, 0f)));
-
-		//Forward to launch position
-		execute(drivetrain, new Drivetrain.Move(new Vector2(0f, 53f)));
-		execute(wobbleGrabber, new WobbleGrabber.Move(WobbleGrabber.Position.GRAB));
-		execute(wobbleGrabber, new WobbleGrabber.Grab(false));
-		execute(wobbleGrabber, new WobbleGrabber.Move(WobbleGrabber.Position.RESET));
-		execute(drivetrain, new Drivetrain.Move(new Vector2(0f,6f)));
-
-		wait(0.5f);
-
-		execute(drivetrain, new Drivetrain.Move(new Vector2(25f, 0f)));
-		execute(drivetrain, new Drivetrain.Drive(new Vector2(1f, 0f), 0.55f));
-
-		wait(0.7f);
+		execute(drivetrain, new Drivetrain.Drive(new Vector2(1f, 0f), .7f));
+		wait(.7f);
 		execute(drivetrain, new Drivetrain.Drive(Vector2.zero));
 		execute(drivetrain, new Drivetrain.Reset());
 
