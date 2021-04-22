4e35b24e6a187608aad8ff56adb11d3d5153318f
MMXXXVIII <henhaodegaigai@gmail.com>
2021-04-21 21:45:54 -0400

    Added wobble sequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Behaviors/TeleOpSequencesI5.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Behaviors/TeleOpSequencesI5.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Behaviors;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -29,8 +30,9 @@ public class TeleOpSequencesI5 extends Behavior
 	{
 		super.awake(hardwareMap);
 
+		opMode.input.registerButton(Input.Source.CONTROLLER_1, Input.Button.B);
 		opMode.input.registerButton(Input.Source.CONTROLLER_1, Input.Button.X);
-		opMode.input.registerButton(Input.Source.CONTROLLER_1, Input.Button.Y);
+		opMode.input.registerButton(Input.Source.CONTROLLER_2, Input.Button.Y);
 
 		opMode.input.registerButton(Input.Source.CONTROLLER_1, Input.Button.LEFT_BUMPER);
 		opMode.input.registerButton(Input.Source.CONTROLLER_1, Input.Button.RIGHT_BUMPER);
@@ -41,11 +43,12 @@ public class TeleOpSequencesI5 extends Behavior
 	{
 		super.update();
 
-		if (opMode.input.getButtonDown(Input.Source.CONTROLLER_1, Input.Button.X)) opMode.assignSequence(null);
+		if (opMode.input.getButtonDown(Input.Source.CONTROLLER_1, Input.Button.B)) opMode.assignSequence(null);
 
 		if (!opMode.hasSequence())
 		{
-			if (opMode.input.getButtonDown(Input.Source.CONTROLLER_1, Input.Button.Y)) opMode.assignSequence(new PowerShotsSequence(opMode));
+			if (opMode.input.getButtonDown(Input.Source.CONTROLLER_1, Input.Button.X)) opMode.assignSequence(new PowerShotsSequence(opMode));
+			if (opMode.input.getButtonDown(Input.Source.CONTROLLER_2, Input.Button.Y)) opMode.assignSequence(new WobbleDropSequence(opMode));
 
 			if (opMode.input.getButtonDown(Input.Source.CONTROLLER_1, Input.Button.LEFT_BUMPER)) opMode.assignSequence(new LaunchSequence(opMode, false));
 			if (opMode.input.getButtonDown(Input.Source.CONTROLLER_1, Input.Button.RIGHT_BUMPER)) opMode.assignSequence(new LaunchSequence(opMode, true));
@@ -102,6 +105,47 @@ public class TeleOpSequencesI5 extends Behavior
 		{
 			DrivetrainI5 drivetrain = opMode.getBehavior(DrivetrainI5.class);
 			powerShots(drivetrain.getDrive().getPoseEstimate());
+		}
+	}
+
+	private static class WobbleDropSequence extends CommonSequence
+	{
+		public WobbleDropSequence(OpModeBase opMode)
+		{
+			super(opMode);
+		}
+
+		@Override
+		protected void queueJobs()
+		{
+			DrivetrainI5 drivetrain = opMode.getBehavior(DrivetrainI5.class);
+			WobbleGrabberI5 grabber = opMode.getBehavior(WobbleGrabberI5.class);
+			SampleMecanumDrive drive = drivetrain.getDrive();
+
+			Pose2d start = drive.getPoseEstimate();
+
+			Trajectory driveToDrop = drive.trajectoryBuilder(start)
+					.splineToSplineHeading(new Pose2d(-63f, start.getY(), Math.toRadians(-90f)), Math.toRadians(180f)).build();
+
+			buffer(grabber, new WobbleGrabberI5.Move(WobbleGrabberI5.Mode.HIGH));
+			buffer(drivetrain, new DrivetrainI5.Follow(driveToDrop));
+
+			execute();
+
+			execute(grabber, new WobbleGrabberI5.Move(WobbleGrabberI5.Mode.DROP));
+			execute(grabber, new WobbleGrabberI5.Grab(false));
+
+			wait(0.3f);
+			execute(grabber, new WobbleGrabberI5.Grab(true));
+
+			Trajectory moveAway = drive.trajectoryBuilder(driveToDrop.end())
+					.splineTo(driveToDrop.end().vec().plus(new Vector2d(20f, -20f)), Math.toRadians(0f)).build();
+
+
+			buffer(grabber, new WobbleGrabberI5.Move(WobbleGrabberI5.Mode.FOLD));
+			buffer(drivetrain, new DrivetrainI5.Follow(moveAway));
+
+			execute();
 		}
 	}
 }
